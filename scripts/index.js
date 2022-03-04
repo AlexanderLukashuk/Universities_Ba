0d0b6d8129a7ff9dc04e3dfc3e31a72d59c4c6f3
AlexanderLukashuk <upiter465@gmail.com>
2022-03-04 11:08:40 +0600

    Добалвено background-image и padding для элементов в header

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,8 @@
-// let page_footer = document.createElement('div');
-// page_footer.className = "page-footer";
-// let container = document.createElement('div');
-// page_footer.append(container);
-// document.body.append(page_footer);
-// page_footer.style.backgroundColor = red;
-
-// $('button[name="getBucks"]').click(function () {
-//     location.href = "help.html";
-// });
+$(".page-main").css({
+    "background-image": "url('img/Nazarbayev_University.jpg')",
+    "background-repeat": "no-repeat",
+    "background-position-x": "right",
+    "padding-left": "150px",
+    "background-size": "736px 800px",
+    "backgroundColor": "#F4F9E2"
+});
