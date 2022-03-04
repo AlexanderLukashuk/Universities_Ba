751fb541148e584c85272a75c25585d009505720
AlexanderLukashuk <upiter465@gmail.com>
2022-03-04 20:39:44 +0600

    Добавлены обработчики событий click на кнопки, которые в карусели, чтобы открывать соответсвующие видео, добавлены id этим кнопкам, чтобы было легче присвоить обработчик событий click, подключен данный скрипт в videos.html

---
+++ scripts/videos.js
@@ -0,0 +1,39 @@
+$("#this_is_mit").click(function() {
+    window.open("https://www.youtube.com/watch?v=KIop1hxFliQ");
+});
+
+$("#how_to_enroll_in_mit").click(function() {
+    window.open("https://www.youtube.com/watch?v=23HnjwTq4kA");
+});
+
+$("#mit_students_life").click(function() {
+    window.open("https://www.youtube.com/watch?v=-ZNm7EYUe-M&pp=ugMICgJydRABGAE%3D");
+});
+
+$("#this_is_stanford").click(function() {
+    window.open("https://www.youtube.com/watch?v=0qHWub21h5c");
+});
+
+$("#inside_stanford_university").click(function() {
+    window.open("https://www.youtube.com/watch?v=emBoDloCze8");
+});
+
+$("#stanford_vlog").click(function() {
+    window.open("https://www.youtube.com/watch?v=n54HxRnieYw");
+});
+
+$("#we_are_harvard").click(function() {
+    window.open("https://www.youtube.com/watch?v=0hrvt1uuEyA");
+});
+
+$("#inside_harvard").click(function() {
+    window.open("https://www.youtube.com/watch?v=bO4RoQL9H8I");
+});
+
+$("#harvard_vlog").click(function() {
+    window.open("https://www.youtube.com/watch?v=fK64nbhZlnY");
+});
+
+$("#harvard_students_life").click(function() {
+    window.open("https://www.youtube.com/watch?v=XkFulQe9EVE");
+});
