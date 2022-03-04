b11f51ffacfb4fe2d0f3e0d5659fe96f1568907d
AlexanderLukashuk <upiter465@gmail.com>
2022-03-04 20:48:38 +0600

    Добавлены обработчики событий на кнопки в header чтобы перемещаться между страницами: home, the best universities, videos

---
+++ scripts/header.js
@@ -0,0 +1,11 @@
+$("#home").click(function() {
+    window.open("index.html", "_self");
+});
+
+$("#top_universities").click(function() {
+    window.open("universities.html", "_self");
+});
+
+$("#videos").click(function() {
+    window.open("videos.html", "_self");
+});
