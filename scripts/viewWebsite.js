a3648b6c11bd5680a25e281564f548abef5e51c6
AlexanderLukashuk <upiter465@gmail.com>
2022-03-04 21:22:15 +0600

    Добавлены id для тэгов a на странице The Best Universities, добавлен скрипт с обработчиками событий открытия при нажатии, которые открывают сайты университетов

---
+++ scripts/viewWebsite.js
@@ -0,0 +1,13 @@
+// $("#mit").attr('href', 'https://www.mit.edu/');
+
+$("#mit").click(function() {
+    window.open("https://www.mit.edu/");
+});
+
+$("#stanford").click(function() {
+    window.open("https://www.stanford.edu/");
+});
+
+$("#harvard").click(function() {
+    window.open("https://www.harvard.edu/");
+});
